f67b87f7b75667600996c8d872062278b92ef0a9
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-06 08:09:53 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -97,7 +97,7 @@ screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 # setting port ssh
 cd
 sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
-sed -i '/Port 22/a Port 143' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 53' /etc/ssh/sshd_config
 service ssh restart
 
 # install dropbear
@@ -186,30 +186,30 @@ service dropbear restart
 # download script
 cd /usr/bin
 wget -O menu "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/menu.sh"
-wget -O crear "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/user-add.sh"
-wget -O prueva "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/trial.sh"
-wget -O eliminar "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/hapus.sh"
-wget -O online "https://raw.githubusercontent.com/maicolxd/script-vps-/master/user-login.sh"
-wget -O lista "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/user-list.sh"
-wget -O expirado "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/delexp.sh"
+wget -O user-add "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/user-add.sh"
+wget -O trial "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/trial.sh"
+wget -O user-del "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/hapus.sh"
+wget -O user-login "https://raw.githubusercontent.com/maicolxd/script-vps-/master/user-login.sh"
+wget -O user-list "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/user-list.sh"
+wget -O expdel "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/delexp.sh"
 wget -O reiniciar "https://raw.githubusercontent.com/kholizsivoi/script/master/resvis.sh"
 wget -O speedtest "https://raw.githubusercontent.com/kholizsivoi/script/master/speedtest_cli.py"
 wget -O info "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/info.sh"
-wget -O mas "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/about.sh"
+wget -O about "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/about.sh"
 
 echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
 
 chmod +x menu
-chmod +x crear
-chmod +x prueva
-chmod +x eliminar
-chmod +x online
-chmod +x lista
+chmod +x user-add
+chmod +x trial
+chmod +x user-del
+chmod +x user-login
+chmod +x user-list
 chmod +x reiniciar
 chmod +x speedtest
 chmod +x info
-chmod +x expirado
-chmod +x mas
+chmod +x expdel
+chmod +x about
 
 # finishing
 cd
@@ -233,5 +233,5 @@ figlet MSC VIP PERU
 figlet TU VPS 
 figlet FUE 
 figlet CONFIGURADO
-mas
+about
 menu
