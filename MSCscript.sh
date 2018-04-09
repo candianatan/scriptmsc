5c3414831da51acedf55074a0c6fddd73de6a79d
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-09 11:21:55 -0500

    Update MSCscript.sh

--- MSCscript.sh
+++ MSCscript.sh
@@ -1,6 +1,6 @@
 # install Script msc
 cd
 # menu 1 msc
-wget https://www.dropbox.com/s/u3f2usa2b5v29p5/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
-# menu 2 msc
+wget https://www.dropbox.com/s/bpnms0qeqh4rgsb/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
+#   menu2 ms
 # install Script2 para la próxima 
