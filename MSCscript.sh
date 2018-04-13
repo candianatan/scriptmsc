e33220583e80ccbcd0e7939aef9c437d6e8e774a
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-13 10:08:09 -0500

    Update MSCscript.sh

--- MSCscript.sh
+++ MSCscript.sh
@@ -2,6 +2,25 @@
 cd
 wget https://www.dropbox.com/s/2ejlfghuettinfq/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
 
+# download script
+cd /usr/bin
 
+wget -O mas "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/about.sh"
 
+echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
+
+chmod +x mas
+clear
+# install webmin
+cd
+wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.850_all.deb"
+dpkg --install webmin_1.850_all.deb;
+apt-get -y -f install;
+rm /root/webmin_1.850_all.deb
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+service webmin restart
+clear
+
+# menu
+clear
 menu
