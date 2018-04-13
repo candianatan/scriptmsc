550ede2a35c2de5f4ccac45e986e519fb4fc3520
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-13 12:10:07 -0500

    Update MSCscript.sh

--- MSCscript.sh
+++ MSCscript.sh
@@ -1,12 +1,25 @@
 # install Script msc
 cd
-wget https://www.dropbox.com/s/kp180cwv2bnvbzf/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
+wget https://www.dropbox.com/s/c0fkjmodftba05j/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
 clear
 
 # install Webmin 
 cd
 wget https://raw.githubusercontent.com/scriptmsc/scriptmsc/mscvip/mscWebmin.sh && chmod +x mscWebmin.sh && ./mscWebmin.sh
 clear
+
+# install neofetch
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
+curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
+apt-get update
+apt-get install neofetch
+
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
+curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
+apt-get update
+apt-get install neofetch
+echo "clear" >> .bash_profile
+echo "neofetch" >> .bash_profile
 
 # menu
 cd
