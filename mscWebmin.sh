8599dd447d5f09845b16ffd6ee8675f5e9258f52
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-13 12:32:09 -0500

    Update mscWebmin.sh

--- mscWebmin.sh
+++ mscWebmin.sh
@@ -18,3 +18,17 @@ sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 clear
 service webmin restart
 clear
+
+# install neofetch
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
+curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
+apt-get update
+apt-get install neofetch
+
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
+curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
+apt-get update
+apt-get install neofetch
+echo "clear" >> .bash_profile
+echo "menu" >> .bash_profile
+clear
