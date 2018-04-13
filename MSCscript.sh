896a1f8a7e4b999bb2a2a724270c6b7a13794947
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-13 12:06:52 -0500

    Update MSCscript.sh

--- MSCscript.sh
+++ MSCscript.sh
@@ -1,11 +1,12 @@
 # install Script msc
 cd
-wget https://www.dropbox.com/s/7mruqaukhycky6y/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
+wget https://www.dropbox.com/s/kp180cwv2bnvbzf/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
 clear
 
 # install Webmin 
 cd
-wget https://raw.githubusercontent.com/scriptmsc/scriptmsc/blob/mscvip/mscWebmin.sh && chmod +x mscWebmin.sh && ./mscWebmin.sh
+wget https://raw.githubusercontent.com/scriptmsc/scriptmsc/mscvip/mscWebmin.sh && chmod +x mscWebmin.sh && ./mscWebmin.sh
+clear
 
 # menu
 cd
