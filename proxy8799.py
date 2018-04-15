5b7e9f7e81357cde7e293ea63d9ff118acc84f3d
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-14 23:47:35 -0500

    Update proxy8799.py

--- proxy8799.py
+++ proxy8799.py
@@ -61,7 +61,7 @@ class SimpleHTTPProxyHandler(BaseHTTPRequestHandler):
             conn = socket.create_connection(address)
         except socket.error:
             return
-        self.send_response(200, '<font color="red">YO</font> <font color="blue">MSC PERU</font>')
+        self.send_response(200, '<font color="blue">AQUÍ</font> <font color="blue">MSC PERU REPORTÁNDOSE</font>')
         self.send_header('Connection', 'close')
         self.end_headers()
 
