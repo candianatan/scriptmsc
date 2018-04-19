0e0ed9e5739ac918198f9512c17d55468ad9f030
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-18 22:07:08 -0500

    Update mscWebmin.sh

--- mscWebmin.sh
+++ mscWebmin.sh
@@ -8,17 +8,6 @@ echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
 chmod +x mas
 clear
 
-# install webmin
-cd
-wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.850_all.deb"
-dpkg --install webmin_1.850_all.deb;
-apt-get -y -f install;
-rm /root/webmin_1.850_all.deb
-sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
-clear
-service webmin restart
-clear
-
 # install neofetch
 echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
 curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
