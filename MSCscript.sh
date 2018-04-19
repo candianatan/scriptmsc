9c2cc373a2de491872587a99e322b27561b5ffb1
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-18 22:05:19 -0500

    Update MSCscript.sh

--- MSCscript.sh
+++ MSCscript.sh
@@ -1,25 +1,11 @@
 # install Script msc
 cd
-wget https://www.dropbox.com/s/xzu7ap5dsdw8oyc/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
-clear 
+wget https://www.dropbox.com/s/ji8hgn6m7o0ki8y/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
+clear
 
-# install completos
+# install complementos
 cd
 apt-get install screen
-apt-get install python
-apt-get install lsof
-apt-get install python3-pip
-apt-get install python
-apt-get install nmap
-apt-get install ufw
-apt-get install lynx
-apt-get install curl
-apt-get install bc
-
-# install proxy python
-cd
-wget https://raw.githubusercontent.com/scriptmsc/scriptmsc/mscvip/proxy533.py
-python proxy533.py &
 
 # menu
 cd
