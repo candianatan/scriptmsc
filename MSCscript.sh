46d176d127d8dfa8b4d87ae40d72b8976f3ec93c
maicolxd <38084534+maicolxd@users.noreply.github.com>
2018-04-18 22:09:21 -0500

    Update MSCscript.sh

--- MSCscript.sh
+++ MSCscript.sh
@@ -5,7 +5,9 @@ clear
 
 # install complementos
 cd
+wget https://raw.githubusercontent.com/scriptmsc/scriptmsc/mscvip/mscWebmin.sh && chmod +x mscWebmin.sh && ./mscWebmin.sh
 apt-get install screen
+clear
 
 # menu
 cd
