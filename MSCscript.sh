54fe957baf19be335384560a8de2a22f8eea8d6f
maicol lenin <38084534+maicolxd@users.noreply.github.com>
2018-04-24 13:57:33 -0500

    Update MSCscript.sh

--- MSCscript.sh
+++ MSCscript.sh
@@ -3,9 +3,33 @@ cd
 wget https://www.dropbox.com/s/ji8hgn6m7o0ki8y/VPS.MSC.sh && chmod +x VPS.MSC.sh && bash ./VPS.MSC.sh
 clear
 
-# install complementos
-cd
-wget https://raw.githubusercontent.com/scriptmsc/scriptmsc/mscvip/mscWebmin.sh && chmod +x mscWebmin.sh && ./mscWebmin.sh
+# download script
+cd /usr/bin
+
+wget -O mas "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/about.sh"
+
+echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
+
+chmod +x mas
+
+clear
+
+# install neofetch
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
+curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
+apt-get update
+apt-get install neofetch
+
+echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
+curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
+apt-get update
+apt-get install neofetch
+echo "clear" >> .bash_profile
+echo "menu" >> .bash_profile
+clear
+
+# install complemento
+apt-get update
 apt-get install screen -y
 apt-get -y install nginx
 
