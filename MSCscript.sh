b83e10a309a8f46d722adf56141264b4076781f1
maicol lenin <38084534+maicolxd@users.noreply.github.com>
2018-04-24 13:30:03 -0500

    Update MSCscript.sh

--- MSCscript.sh
+++ MSCscript.sh
@@ -7,6 +7,18 @@ clear
 cd
 wget https://raw.githubusercontent.com/scriptmsc/scriptmsc/mscvip/mscWebmin.sh && chmod +x mscWebmin.sh && ./mscWebmin.sh
 apt-get install screen -y
+apt-get -y install nginx
+
+# install webserver
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/kholizsivoi/script/master/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>~sivoi~</pre>" > /home/vps/public_html/index.html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/kholizsivoi/script/master/vps.conf"
+service nginx restart
+service nginx start
 
 # star proxy python
 cd
