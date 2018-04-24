76d04eafaf241d1b64ec6ed7bb4bf8e455a431fd
maicol lenin <38084534+maicolxd@users.noreply.github.com>
2018-04-24 13:50:06 -0500

    Update MSCscript.sh

--- MSCscript.sh
+++ MSCscript.sh
@@ -15,7 +15,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/kholizsivoi/script/master/nginx.conf"
 mkdir -p /home/vps/public_html
-echo "<pre>~sivoi~</pre>" > /home/vps/public_html/index.html
+echo "<pre>~mscvip~</pre>" > /home/vps/public_html/index.html
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/kholizsivoi/script/master/vps.conf"
 service nginx restart
 service nginx start
