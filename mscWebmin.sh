6ab6e93e47fca9a6c630a05569bf4712b45fdb5b
maicol lenin <38084534+maicolxd@users.noreply.github.com>
2018-05-05 19:43:03 -0500

    Update mscWebmin.sh

--- mscWebmin.sh
+++ mscWebmin.sh
@@ -1,8 +1,8 @@
 # install webmin
 cd
-wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.850_all.deb"
-dpkg --install webmin_1.850_all.deb;
+wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.881_all.deb"
+dpkg --install webmin_1.881_all.deb;
 apt-get -y -f install;
-rm /root/webmin_1.850_all.deb
+rm /root/webmin_1.881_all.deb
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 service webmin restart
