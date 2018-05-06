5773719c0f76b5701759d73f5f69c159081899cd
maicol lenin <38084534+maicolxd@users.noreply.github.com>
2018-05-05 19:17:22 -0500

    Update mscWebmin.sh

--- mscWebmin.sh
+++ mscWebmin.sh
@@ -1,24 +1,8 @@
-# download script
-cd /usr/bin
-
-wget -O mas "https://raw.githubusercontent.com/maicolxd/script-vps-/mscvip-1/about.sh"
-
-echo "0 0 * * * root /sbin/reboot" > /etc/cron.d/reboot
-
-chmod +x mas
-
-clear
-
-# install neofetch
-echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
-curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
-apt-get update
-apt-get install neofetch
-
-echo "deb http://dl.bintray.com/dawidd6/neofetch jessie main" | tee -a /etc/apt/sources.list
-curl "https://bintray.com/user/downloadSubjectPublicKey?username=bintray"| apt-key add -
-apt-get update
-apt-get install neofetch
-echo "clear" >> .bash_profile
-echo "menu" >> .bash_profile
-clear
+# install webmin
+cd
+wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.850_all.deb"
+dpkg --install webmin_1.850_all.deb;
+apt-get -y -f install;
+rm /root/webmin_1.850_all.deb
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+service webmin restart
