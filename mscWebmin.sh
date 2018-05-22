e614d9d578205466ed32a0f7598e6585a67f3ecd
maicol lenin <38084534+maicolxd@users.noreply.github.com>
2018-05-22 08:11:51 -0500

    Update mscWebmin.sh

--- mscWebmin.sh
+++ mscWebmin.sh
@@ -6,3 +6,15 @@ apt-get -y -f install;
 rm /root/webmin_1.881_all.deb
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 service webmin restart
+
+
+# install webserver nginx
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/kholizsivoi/script/master/nginx.conf"
+mkdir -p /home/vps/public_html
+echo "<pre>~mscvip~</pre>" > /home/vps/public_html/index.html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/kholizsivoi/script/master/vps.conf"
+service nginx restart
+service nginx start
